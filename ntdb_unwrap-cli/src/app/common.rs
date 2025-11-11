bbb36c61f09c69a577be1e5e70c9cfd4d667bec6
artiga033 <artiga033@hotmail.com>
2025-11-12 00:46:09 +0800

    release: 0.2.2

--- ntdb_unwrap-cli/src/app/common.rs
+++ ntdb_unwrap-cli/src/app/common.rs
@@ -37,9 +37,10 @@ pub fn bootstrap(matches: &ArgMatches) -> Result<Bootstrap> {
                     input.clear();
                     std::io::stdin().read_line(&mut input).unwrap();
                     if let Ok(i) = input.trim().parse::<usize>()
-                        && i < db_files.len() {
-                            break db_files.into_iter().nth(i).unwrap();
-                        }
+                        && i < db_files.len()
+                    {
+                        break db_files.into_iter().nth(i).unwrap();
+                    }
                     println!("无效输入，请重试：");
                 }
             }
